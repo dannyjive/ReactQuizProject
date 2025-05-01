77bafd943af37fbd6912e9e2d273dda739b3e761
Dan Finley <44242241+dannyjive@users.noreply.github.com>
2025-04-30 17:49:56 -0700

    set up quiz and header modules

---
+++ src/components/Quiz.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+export default function Quiz(){
+
+    const [question, setQuestion] = useState();
+
+    return
+// show currently active question to user
+
+ <question/> ?? <question/> : <newQuestion/>
+
+// when question is answered. Switch to different question.
+}
