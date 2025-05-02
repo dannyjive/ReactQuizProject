c916025152b32e926d0e01b6f525cab8a6314f23
Dan Finley <44242241+dannyjive@users.noreply.github.com>
2025-05-01 18:23:53 -0700

    added quiz usestate

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import logoImg from '../assets/quiz-logo.png'
 
+
 export default function Header(){
     return <header>
         <img src={logoImg} alt='quiz logo'/>
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,13 +1,29 @@
 import { useState } from "react";
+import QUESTIONS from "../questions";
 
-export default function Quiz(){
+export default function Quiz() {
+  const [userAnswer, setUserAnswer] = useState([]);
 
-    const [question, setQuestion] = useState();
+  const activeQuestionIndex = userAnswer.length;
 
-    return
-// show currently active question to user
+  function handleSelectAnswer(selectedAnswer) {
+    setUserAnswer((prevUserAnswer) => {
+        return [...prevUserAnswer, selectedAnswer]
+    });
+  }
 
- <question/> ?? <question/> : <newQuestion/>
-
-// when question is answered. Switch to different question.
+  return (
+    <div id="quiz">
+    <div id={"question"}>
+      <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+      <ul id="answers">
+        {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+          <li key={answer} className="answer">
+            <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+    </div>
+  );
 }
